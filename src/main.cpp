b1621dcc52e13b7ca0f2247c87cd550ef9abec15
Daniel TB <varig203@gmail.com>
2024-10-04 15:16:52 -0700

    Starting tuning of the angular controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
+#include "pros/adi.hpp"
 
 // Creating the Motor groups
 pros::MotorGroup left_motors({9, 19}, pros::MotorGearset::blue); // left motors on ports 1, 11
 pros::MotorGroup right_motors({-1, -11}, pros::MotorGearset::blue); // right motors on ports 9, 19
 
 // Creating the IMU port
-pros::Imu imu(20);
+pros::Imu imu(21);
+// Creating the Vertical tracker
+pros::adi::Encoder vertical_encoder('A', 'B', true);
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::NEW_275, +1);
 
 // drivetrain settings
 lemlib::Drivetrain drivetrain(&left_motors, // left motor group
@@ -17,7 +21,7 @@ lemlib::Drivetrain drivetrain(&left_motors, // left motor group
                               2 // horizontal drift is 2 (for now)
 );
 
-lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
                             nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
                             nullptr, // horizontal tracking wheel 1
                             nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
@@ -40,11 +44,11 @@ lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
 lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
                                               0, // integral gain (kI)
                                               10, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in degrees
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in degrees
-                                              500, // large error range timeout, in milliseconds
+                                              0, // anti windup
+                                              0, // small error range, in degrees
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in degrees
+                                              0, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
 );
 
@@ -73,10 +77,10 @@ void on_center_button() {
   static bool pressed = false;
   pressed = !pressed;
   if (pressed) {
-    pros::lcd::set_text(6, "Running Autonomous");
+    pros::lcd::set_text(7, "Running Autonomous");
     autonomous();
   } else {
-    pros::lcd::clear_line(6);
+    pros::lcd::clear_line(7);
   }
 }
 
@@ -101,8 +105,12 @@ void initialize() {
             // IMU readings
             pros::lcd::print(3, "IMU Heading: %f", imu.get_heading()); // IMU heading
             pros::lcd::print(4, "Gyro Rate: %f", imu.get_gyro_rate()); // Angular velocity
-            
-			// delay to save resources
+
+            // print measurements from the adi encoder
+            pros::lcd::print(5, "ADI Encoder: %i", vertical_encoder.get_value());
+            // print measurements from the rotation sensor
+			
+            // delay to save resources
             pros::delay(20);
         }
     });
@@ -159,26 +167,6 @@ void autonomous() {
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-/** void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
-
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
-} 
-*/
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 void opcontrol() {

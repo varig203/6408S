6b1e045b32415649f44dc505b3d39f229e1b5f62
Daniel TB <varig203@gmail.com>
2024-12-17 17:09:48 -0800

    Refactored the code

--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,10 @@
 #include "main.h"
-#include "lemlib/api.hpp" // IWYU pragma: keep
-#include "pros/abstract_motor.hpp"
-#include "pros/adi.hpp"
-#include "pros/llemu.hpp"
-#include "pros/misc.h"
-#include "pros/misc.hpp"
-#include "pros/motors.h"
-#include "pros/rtos.hpp"
+#include "robot/hardware.hpp"
 #include <iostream>
 #include <ostream>
-
-// Creating the Motor groups
-pros::MotorGroup left_motors({-14, -15, -19}, pros::MotorGearset::blue); // left motors on ports 14, 15, 19
-pros::MotorGroup right_motors({13, 16, 17}, pros::MotorGearset::blue);   // right motors on ports 13, 16, 17
-pros::Motor      primary_intake(20, pros::MotorGearset::blue);           // Primary Intake on port 20
-pros::Motor      secondary_intake(18, pros::MotorGearset::blue);         // Secondary Intake on port 18
-
-// Creating controller/Pistons
-pros::Controller      controller(pros::E_CONTROLLER_MASTER); // Initialize controller
-pros::adi::DigitalOut pistonExtendMogo('A');                 // Initialize the solenoid for extending MOGO
-pros::adi::DigitalOut pistonRetractMogo('B');                // Initialize the solenoid for retracting MOGO
-pros::adi::DigitalOut pistonLB('C');                         // Initialize the LB mech
-
-// Creating Sensors
-pros::Imu imu(12); // IMU
-
-// setting up the vertical Rotation Sensor
-pros::Rotation        vertical_encoder(11);
-lemlib::TrackingWheel vertical_tracking_wheel(&vertical_encoder, lemlib::Omniwheel::NEW_275, -2.5);
-
-// drivetrain settings
-lemlib::Drivetrain drivetrain(&left_motors,             // left motor group
-                              &right_motors,            // right motor group
-                              16,                       // 10 inch track width
-                              lemlib::Omniwheel::NEW_4, // using new 4" omnis
-                              600,                      // drivetrain rpm is 600
-                              2                         // horizontal drift is 2 (for now)
-);
-
-lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1
-                            nullptr,                  // vertical tracking wheel 2, set to nullptr as we are using IMEs
-                            nullptr,                  // horizontal tracking wheel 1, set to nullptr as we don't have any
-                            nullptr,                  // horizontal tracking wheel 2, set to nullptr as we don't have any
-                            &imu                      // inertial sensor
-);
-
-// lateral PID controller
-lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
-                                              0,  // integral gain (kI)
-                                              3,  // derivative gain (kD)
-                                              0,  // anti windup
-                                              0,  // small error range, in inches
-                                              0,  // small error range timeout, in milliseconds
-                                              0,  // large error range, in inches
-                                              0,  // large error range timeout, in milliseconds
-                                              0   // maximum acceleration (slew)
-);
-
-// angular PID controller
-lemlib::ControllerSettings angular_controller(2,  // proportional gain (kP)
-                                              0,  // integral gain (kI)
-                                              10, // derivative gain (kD)
-                                              0,  // anti windup
-                                              0,  // small error range, in degrees
-                                              0,  // small error range timeout, in milliseconds
-                                              0,  // large error range, in degrees
-                                              0,  // large error range timeout, in milliseconds
-                                              0   // maximum acceleration (slew)
-);
-
-// input curve for throttle input during driver control
-lemlib::ExpoDriveCurve throttle_curve(3,    // joystick deadband out of 127
-                                      10,   // minimum output where drivetrain will move out of 127
-                                      1.019 // expo curve gain
-);
-
-// input curve for steer input during driver control
-lemlib::ExpoDriveCurve steer_curve(3,    // joystick deadband out of 127
-                                   10,   // minimum output where drivetrain will move out of 127
-                                   1.019 // expo curve gain
-);
-
-// create the chassis
-lemlib::Chassis chassis(drivetrain,         // drivetrain settings
-                        lateral_controller, // lateral PID settings
-                        angular_controller, // angular PID settings
-                        sensors,            // odometry sensors
-                        &steer_curve,
-                        &throttle_curve);
+#include "pros/rtos.hpp"
+#include "pros/misc.h"
+#include "pros/llemu.hpp"
 
 void on_center_button() {
     static bool pressed = false;
@@ -153,30 +70,6 @@ void disabled() {
  * starts.
  */
 void competition_initialize() {}
-
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task||I
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-ASSET(test_txt); // Auto test
-ASSET(test2_txt);
-
-void autonomous() {
-    // Simple straight line
-    chassis.setBrakeMode(pros::E_MOTOR_BRAKE_BRAKE);
-    chassis.setPose(0, 0, 0);
-    chassis.setBrakeMode(pros::E_MOTOR_BRAKE_BRAKE);
-    chassis.follow(test_txt, 20, 2000);
-    pros::delay(600);
-    chassis.cancelAllMotions();
-}
 
 void solenoidControl_fn() {                                   // Controls all the solenoids on the robot in a single task
     std::cout << "Solenoid Control Starting..." << std::endl; // Print into PROS Terminal for debugging

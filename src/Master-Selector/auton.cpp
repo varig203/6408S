0d592e9591b21fb4a1082aa8b039f6c878fa3e4b
Daniel TB <varig203@gmail.com>
2025-01-10 11:30:44 -0800

    Gave up

--- src/Master-Selector/auton.cpp
+++
@@ -1,5 +0,0 @@
-#include "Master-Selector/auton.hpp"
-
-
-ms::Auton::Auton(std::string name, const std::function<void()>& callback)
-    : name(name), callback(callback) {}
--- src/Master-Selector/category.cpp
+++
@@ -1,5 +0,0 @@
-#include "Master-Selector/category.hpp"
-
-
-ms::Category::Category(std::string name, const std::vector <Auton>& autons)
-    : name(name), autons(autons) {}
